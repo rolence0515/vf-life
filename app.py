98d60ff90a94456d9431273f90163b5248f4fbed
rolence <rolence@reddoor.com.tw>
2025-04-30 23:44:07 +0800

    deploy

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/orders')
+def orders():
+    # 假資料直接在模板中渲染
+    return render_template('orders.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
