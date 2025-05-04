4639b3db3b4dad63d4e4b3ba089a7145e0ab8e74
rolence <rolence@reddoor.com.tw>
2025-05-04 21:06:35 +0800

    Feat 新增多個管理員功能頁面
    移除路由上的管理員限制註釋以開放更多功能頁面訪問
    新增多個管理員相關的模板頁面：admin_users, series等
    更新側邊欄導航以反映新增加的功能頁面
    
    修正頁面以繼承 admin_base.html 並優化結構

--- admin_blueprint.py
+++ admin_blueprint.py
@@ -4,6 +4,31 @@ from lib.login_required import login_required, admin_user_required
 admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
 
 @admin_bp.route('/')
-@admin_user_required
+# @admin_user_required
 def admin_index():
     return render_template('admin_sample.html')
+
+@admin_bp.route('/user')
+# @admin_user_required
+def admin_user():
+    return render_template('admin_user.html')
+
+@admin_bp.route('/user_videos')
+# @admin_user_required
+def admin_user_videos():
+    return render_template('admin_user_videos.html')
+
+@admin_bp.route('/series')
+# @admin_user_required
+def admin_series():
+    return render_template('admin_series.html')
+
+@admin_bp.route('/videos')
+# @admin_user_required
+def admin_videos():
+    return render_template('admin_videos.html')
+
+@admin_bp.route('/admin_users')
+# @admin_user_required
+def admin_admin_users():
+    return render_template('admin_admin_users.html')
